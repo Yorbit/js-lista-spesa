f7c30c8df8c3dcba13d76a0fec35e4eeb3c0417e
Yorbit <marco.95112@hotmail.it>
2022-06-28 15:42:13 +0200

    ended while, extra for and fix css

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ const toshopItem = document.getElementById('list');
 
 let index = 0;
 
+while (index < listItem.length) {
+
+    const newItem = document.createElement('li');
+
+    newItem.classList.add('padding');
+
+    toshopItem.append(newItem);
+
+    newItem.innerHTML = (listItem[index]);
+
+    index++;
+}
+
+// ! CICLO FOR PROVA \\
+// for (let index = 0; index < listItem.length; index++) {
+        
+//     const newItem = document.createElement('li');
+
+//     newItem.classList.add('padding');
+
+//     toshopItem.append(newItem);
+
+//     newItem.innerHTML = (listItem[index]);
+// }
+// CICLO FOR \\
