b189a48d62bbb96141c00f8b5b865e7f55c24ee4
Yorbit <marco.95112@hotmail.it>
2022-06-28 15:30:40 +0200

    added list and css layout

---
+++ script.js
@@ -0,0 +1,8 @@
+const listItem = [
+    'Uova', 'Pane', 'Latte', 'Biscotti', 'Pasta', 'Affettati', 'Formaggio', 'Verdura', 'Frutta'
+]
+
+const toshopItem = document.getElementById('list');
+
+let index = 0;
+
